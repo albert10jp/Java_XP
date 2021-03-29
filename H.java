cee01e8f48aa33ba535c599152940ed34bfd3b38
AlbertJ <74515032+albert10jp@users.noreply.github.com>
2021-03-29 21:43:24 +0800

    heart beat animation with Java
    heart beat animation with Java, simple example to learn while loop, if, and try ... catch

---
+++ H.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.TimeUnit;
+
+public class H {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 9) {
+            System.out.println("\033[1;1H\033[2J\033[?25l");
+            if (i % 2 == 0) {
+                System.out.println("    * * * *           * * * * ");
+                System.out.println("  * * * * * *       * * * * * * ");
+                System.out.println("* * * * * * * *   * * * * * * * * ");
+                System.out.println("* * * * * * * * * * * * * * * * * ");
+                System.out.println("  * * * * * * * * * * * * * * * ");
+                System.out.println("    * * * * * * * * * * * * * ");
+                System.out.println("      * * * * * * * * * * * ");
+                System.out.println("        * * * * * * * * * ");
+                System.out.println("          * * * * * * * ");
+                System.out.println("            * * * * * ");
+                System.out.println("              * * * ");
+                System.out.println("                * ");
+            } else {
+                System.out.println("     * *             * *");
+                System.out.println("   * * * *         * * * *");
+                System.out.println(" * * * * * *     * * * * * *");
+                System.out.println("  * * * * * * * * * * * * *");
+                System.out.println("    * * * * * * * * * * *");
+                System.out.println("      * * * * * * * * *");
+                System.out.println("        * * * * * * *");
+                System.out.println("          * * * * *");
+                System.out.println("            * * *");
+                System.out.println("              *");
+            }
+            try {
+                // delay 5 seconds
+                // TimeUnit.SECONDS.sleep(1);
+                // delay 0.3 second
+                TimeUnit.MICROSECONDS.sleep(300000);
+            } catch (InterruptedException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+            i++;
+        }
+    }
+}
